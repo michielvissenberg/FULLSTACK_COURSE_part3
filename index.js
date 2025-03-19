45bd240d76d94f411bf2f76cf27c667393bb99d3
michielvissenberg <michiel@vissenberg.be>
2025-03-19 19:40:31 +0100

    ex 3.1 - 3.6

---
+++ index.js
@@ -0,0 +1,91 @@
+// VOLGENDE OEF: 3.3 !!!!!!!!!!!!!!!!
+
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  time = new Date()
+  personsNumber = persons.length
+  console.log(time)
+  response.send(`<p>Phonebook has info for ${personsNumber} people</p>
+                 <p>${time}</p>`)
+})
+
+app.get(`/api/persons/:id`, (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
+
+  if(person){
+    response.json(person)
+  } else {
+    response.status(404).json({error: "Person not found"})
+  }
+})
+
+app.delete(`/api/persons/:id`, (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post(`/api/persons`, (request, response) => {
+  const body = request.body
+
+  if (!(body.name && body.number)) {
+    return response.status(400).json({
+      error: 'make sure to give a name and number'
+    })
+  }
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'person already in database'
+    })
+  }
+
+  const id = String(Math.floor(Math.random()*10000));
+
+  const person = {
+    id: id,
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
